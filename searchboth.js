e434537ffe894da20aee61150c175579371e6240
mirinaru <82160645+mirinaru@users.noreply.github.com>
2021-04-27 22:51:06 +0900

    Update searchboth.js

--- searchboth.js
+++ searchboth.js
@@ -1,30 +1,81 @@
 import { createInfoText } from "./searchresult.js";
 import { testLawData } from "./testData.js";
 
-var $searchInputClick = _$('.search_input').find('a');
-
-$searchInputClick.on('click', searchText );
-
-var _$searchInput = _$('.search_input').find('input');
-
-_$searchInput.keyup(function(e){
-
-if(e.keyCode == 13) {      
-          searchText( );
-     }
-});
-
-
-
-function searchText( $e ){
-     var thisText = $('aside .menu_box .search .search_input input').val();  
-
-   if( thisText == '' ){
-           $('aside .menu_box .search .search_input input').val('').blur();
-           alert('검색어가 없습니다.');
-      } else {
-            window.location.href = (이동시킬도메인 + "/search/search_list.asp?q=" + encodeURIComponent(thisText));                                                                //이동시킬 페이지                     //encodeURIComponent로 변환하여 넘기기 
-
-      }
-
+function searchHandler() {
+  const value = document.getElementById("search").value;
+  alert(value + " 를 검색합니다.");
+  window.open(
+    "http://web2021-1.s3-website.ap-northeast-2.amazonaws.com/searchboth.html" +
+      value,
+    "_blank"
+  );
 }
+
+/**
+ * Create simple card
+ *
+ * ! Consider a more simpler way..
+ * @param {*} data law data
+ * @returns Card div element.
+ */
+const createCard = (data) => {
+  const { id, title, whoCreate, where, when, summary, isCompleted } = data;
+
+  const cardElement = document.createElement("div");
+  const inner = document.createElement("div");
+  const description = document.createElement("div");
+  const titleElement = document.createElement("p");
+  const paragraphElement = document.createElement("p");
+
+  // Set card element attribute
+  cardElement.setAttribute("class", "card");
+  cardElement.style.cursor = "pointer";
+  cardElement.onclick = () => moveToSearchResult(id);
+
+  inner.setAttribute("class", "inner");
+  description.setAttribute("class", "description");
+  titleElement.setAttribute("class", "cardTitle");
+  paragraphElement.setAttribute("class", "cardParagraph");
+
+  const infoList = [whoCreate, where, when, summary];
+  createInfoText(infoList, paragraphElement);
+  const cardTitle = document.createTextNode(title);
+
+  titleElement.appendChild(cardTitle);
+
+  const image = new Image();
+  image.setAttribute("class", "cardImage");
+
+  if (isCompleted) image.src = "./img/complete.png";
+  else image.src = "./img/inprogress.png";
+
+  inner.appendChild(titleElement);
+  description.appendChild(paragraphElement);
+  description.appendChild(image);
+  inner.appendChild(description);
+
+  cardElement.appendChild(inner);
+
+  return cardElement;
+};
+
+/**
+ * Card item view.
+ * ! Modify or remove this function after presentation.
+ */
+window.onload = () => {
+  const CardsPreview = () => {
+    const div = document.getElementById("cardBox");
+    // The number of cards shown in the preview
+    const maxAmount = 12;
+
+    for (let i = 0; i < maxAmount; i++) {
+      const id = i % 3;
+      const currentData = testLawData[id];
+      const card = createCard(currentData);
+      div.appendChild(card);
+    }
+  };
+
+  CardsPreview();
+};
