a074723cd54c6942c87ff00e089defce013f1189
marvin000snu <57986707+marvin000snu@users.noreply.github.com>
2021-05-23 17:24:30 +0900

    add ajax

--- js/people.js
+++ js/people.js
@@ -1,15 +1,25 @@
-function moveToLawSearch(){
-    window.location.href ="./lawsearch.html"
+function moveToLawSearch() {
+  window.location.href = "./lawsearch.html";
 }
 
-function moveToPeopleSearch(){
-    window.location.href ="./people.html"
+function moveToPeopleSearch() {
+  window.location.href = "./people.html";
 }
 
-function moveToPeopleDetail(){
-    window.location.href = "./peopleDetail.html"
+function moveToPeopleDetail() {
+  window.location.href = "./peopleDetail.html";
 }
 
 function moveToHome() {
-    window.location.href = "./index.html";
+  window.location.href = "./index.html";
 }
+
+window.onload = function () {
+  $.ajax({
+    url: "http://3.34.197.145:3002/api/people/getAllPeople", // 클라이언트가 요청을 보낼 서버의 URL 주소
+    type: "GET", // HTTP 요청 방식(GET, POST)
+    dataType: "json" // 서버에서 보내줄 데이터의 타입
+  }).done((res) => {
+    console.log(res);
+  });
+};
